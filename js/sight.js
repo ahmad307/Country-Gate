0feacd36a1b3f65fe130d3bf88841f11453c5025
Ahmad <ahmad.hussain.307@gmail.com>
2020-05-05 00:07:04 +0000

    Configured dynamic loading

--- js/sight.js
+++ js/sight.js
@@ -1,3 +1,15 @@
+var data = {
+    "Eiffel Tower": {
+        "images": [
+            "Eiffel Tower1.jpg",
+            "Eiffel Tower2.jpg",
+            "Eiffel Tower3.jpg",
+            "Eiffel Tower4.jpg"
+        ],
+        "text": "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. Wikipedia"
+    }
+}
+
 function heartFlip() {
     var className = $("#heartIcon").attr("class");
     if (className === "far fa-heart"){
@@ -7,7 +19,34 @@ function heartFlip() {
     }
 }
 
+function fillPage(sight, city) {
+    $("#cityName").html(city);
+
+    $(".sightName").html(sight);
+    $("#sightDescription").html(data[sight]["text"]);
+    $(".header").css("background", `linear-gradient(to bottom, rgba(22, 22, 22, 0.1) 50%,rgba(22, 22, 22, 0.5) 75%, #161616 100%),`
+     + `url("./img/${data[sight]['images'][0]}")`);
+    // $(".header").css("background-image", 'url("./img/Eiffel Tower1.jpg")');
+
+    $("#sightImage1").attr("src", "./img/" + data[sight]["images"][1]);
+    $("#sightImage2").attr("src", "./img/" + data[sight]["images"][2]);
+    $("#sightImage3").attr("src", "./img/" + data[sight]["images"][3]);
+}
+
 $(document).ready(function() {
+    $(document).on("click", function(event) {
+        var text = event.target.textContent;
+        var type = event.target.nodeName;
+        // console.log(text, type);
+    });
+
+    localStorage.setItem("sight", "Eiffel Tower");
+    localStorage.setItem("city", "France");
+    var sight = localStorage.getItem("sight");
+    var city = localStorage.getItem("city");
+    fillPage(sight, city);
+    
+
     $("#feedbackSection").on("click", "button", function() {
         $("#feedbackMessage").toggle();
     });
